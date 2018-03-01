98fce972685e8b0192848c464d07f06bc93f5742
VIOSSAT GEOFFRAY <geoffray.viossat@cpam-rhone.cnamts.fr>
2018-03-01 23:18:24 +0100

    Initialisation du controller de Product

---
+++ src/AppBundle/Controller/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductController extends Controller
+{
+    public function addAction(Request $request)
+    {
+        // Action d'ajout d'un produit
+    }
+
+    public function editAction(Request $request)
+    {
+        // Action de modification d'un produit
+    }
+
+    public function deleteAction(Request $request)
+    {
+        // action de suppression d'un produit
+    }
+}

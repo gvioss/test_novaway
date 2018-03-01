5ec458613a1f3ff95976c674b52123a6394ab7db
VIOSSAT GEOFFRAY <geoffray.viossat@cpam-rhone.cnamts.fr>
2018-03-01 23:21:51 +0100

    Definition des entitees Author, Director et Actor

--- src/AppBundle/Entity/Author.php
+++ src/AppBundle/Entity/Author.php
@@ -1,7 +1,62 @@
 <?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
 /**
- * Created by PhpStorm.
- * User: VIOSSAT-22401
- * Date: 01/03/2018
- * Time: 22:04
+ * @ORM\Entity
+ * @ORM\Table(name="author")
  */
+class Author
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $age;
+
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+}
--- src/AppBundle/Entity/Director.php
+++ src/AppBundle/Entity/Director.php
@@ -1,7 +1,62 @@
 <?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
 /**
- * Created by PhpStorm.
- * User: VIOSSAT-22401
- * Date: 01/03/2018
- * Time: 22:10
+ * @ORM\Entity
+ * @ORM\Table(name="director")
  */
+class Director
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $age;
+
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+}

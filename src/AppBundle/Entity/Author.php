aeaf12ab7cb590e77612a407e28c9abe184a11d8
VIOSSAT GEOFFRAY <geoffray.viossat@cpam-rhone.cnamts.fr>
2018-03-02 22:10:40 +0100

    Getter/Setter du champ id

--- src/AppBundle/Entity/Author.php
+++ src/AppBundle/Entity/Author.php
@@ -32,6 +32,22 @@ class Author
      */
     private $age;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
 
     /**
      * @return mixed

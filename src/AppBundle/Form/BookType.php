4ffc10b73b1ab0f783fe2dd66a33719566057ba1
VIOSSAT GEOFFRAY <geoffray.viossat@cpam-rhone.cnamts.fr>
2018-03-02 22:34:57 +0100

    Paramétrage des champs de BookType

--- src/AppBundle/Form/BookType.php
+++ src/AppBundle/Form/BookType.php
@@ -4,6 +4,11 @@ namespace AppBundle\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +17,32 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('isnb')
-            ->add('title')
+            ->add('isnb', TextType::class, [
+                'label' => 'Numéro ISNB',
+                'required' => true
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
             ->add('author', EntityType::class, [
                 'class' => 'AppBundle\Entity\Author',
                 'choice_label' => 'name',
-                'choice_value' => 'id'
+                'choice_value' => 'id',
+                'label' => 'Auteur'
             ])
-            ->add('release_date')
-            ->add('page_number')
-            ->add('resume')
-            ->add('price')
+            ->add('release_date', DateType::class, [
+                'label' => 'Date de parution',
+                'widget' => 'single_text'
+            ])
+            ->add('page_number', IntegerType::class, [
+                'label' => 'Nombre de page'
+            ])
+            ->add('resume', TextareaType::class, [
+                'label' => 'Résumé'
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix'
+            ])
         ;
     }
 
